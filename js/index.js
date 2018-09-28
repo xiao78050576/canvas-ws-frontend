59f73b93d8b2f7c74d744825d613ab9818ea6db8
xiao78050576 <xiao78050576@gmail.com>
2018-09-29 01:03:25 +0800

    添加查询测试

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		var comment = $("#comment").val();
 		
 		// alert("name:" + name + ";username:" + username + ";password:" + password + ";comment:" + comment);
-		alert("/add?time=" + new Date().getMilliseconds());
+		// alert("/add?time=" + new Date().getMilliseconds());
 		
 		$.ajax({
 			url:"/add?time=" + new Date().getMilliseconds(),
@@ -27,4 +27,28 @@ $(document).ready(function(){
 			}
 		});
 	});
+	
+	$("#execute").click(function(){
+		var id = $("#id").val();
+		
+		// alert("id:" + id);
+		// alert("/getUser?time=" + new Date().getMilliseconds());
+		
+		$.ajax({
+			url:"/getUser?time=" + new Date().getMilliseconds(),
+			async:true,
+			cache:false,
+			type:"GET",
+			data:{
+				id:id
+			},
+			success:function(result, status, xhr){
+				alert("success:" + JSON.stringify(result));
+			},
+			error:function(xhr, status, error){
+				alert("error:" + status);
+			}
+		});
+	});
+
 });
